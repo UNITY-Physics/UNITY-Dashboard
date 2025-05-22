16db7d045fd0d7c697bd84fe3e0ebbdbd5778036
hajerkr <heajer.karoui@gmail.com>
2025-05-22 14:10:55 +0100

    fix

--- main.py
+++ main.py
@@ -42,54 +42,55 @@ try:
                 acqs = [acq for acq in session.acquisitions() if "FISP" in acq.label or ("T2" in acq.label and "AXI" in acq.label)]
                 print(session.label)
                 temp_d = None
-                for acquisition in acqs:
-                    print(acquisition.label)
-                    acquisition = acquisition.reload()
+                if acqs:
+                    for acquisition in acqs:
+                        print(acquisition.label)
+                        acquisition = acquisition.reload()
 
-                    if acquisition.files:
-                        print("Files: ", len(acquisition.files))
-                
-                        json_f = [f for f in acquisition.files if f.name.endswith(".json")]
-                        if json_f:
-                            json_f = json_f[0]
-                            acquisition.download_file(json_f.name, os.path.join(download_path, json_f.name))
-                            with open(os.path.join(download_path, json_f.name),'r') as jf:
-                                sw = json.load(jf)["SoftwareVersions"]
-                                print("SW: ", sw)
-                            
-                        if "FISP" in acquisition.label:
-                            
-                            fisp_f = [f for f in acquisition.files if f.name.endswith(".dcm") or f.name.endswith(".dicom") or f.name.endswith(".zip")][0]
-                            
-                            acquisition.download_file(fisp_f.name, os.path.join(download_path,fisp_f.name))
-                            
-                            if fisp_f.name.endswith('zip'):
-                                zip_path =  os.path.join(download_path,fisp_f.name)
-                                extract_dir = download_path
-                                
-                                with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-                                    zip_ref.extractall(extract_dir)
+                        if acquisition.files:
+                            print("Files: ", len(acquisition.files))
                     
-                                    for root, _, files in os.walk(extract_dir):
-                                        for file in files:
-                                            if file.lower().endswith(".dcm"):
-                                                dcm_path = os.path.join(download_path, root, file)
-                                                ds = pydicom.dcmread(dcm_path)
-                                                temperature = ds.get("PatientComments", None)
-                                                temp_d = re.findall(r'\d+', temperature)  
-                                                
-                                                #print("Temperature: ", temp_d)
-                                                
-                            
-                            else:
-                                ds = pydicom.dcmread(os.path.join(download_path, fisp_f.name))
-                                temperature = ds.get("PatientComments", None)
-                                temp_d = re.findall(r'\d+', temperature)                 
-                                #print("Temperature: ", temp_d)
+                            json_f = [f for f in acquisition.files if f.name.endswith(".json")]
+                            if json_f:
+                                json_f = json_f[0]
+                                acquisition.download_file(json_f.name, os.path.join(download_path, json_f.name))
+                                with open(os.path.join(download_path, json_f.name),'r') as jf:
+                                    sw = json.load(jf)["SoftwareVersions"]
+                                    print("SW: ", sw)
+                                
+                            if "FISP" in acquisition.label:
+                                
+                                fisp_f = [f for f in acquisition.files if f.name.endswith(".dcm") or f.name.endswith(".dicom") or f.name.endswith(".zip")][0]
+                                
+                                acquisition.download_file(fisp_f.name, os.path.join(download_path,fisp_f.name))
+                                
+                                if fisp_f.name.endswith('zip'):
+                                    zip_path =  os.path.join(download_path,fisp_f.name)
+                                    extract_dir = download_path
+                                    
+                                    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                                        zip_ref.extractall(extract_dir)
+                        
+                                        for root, _, files in os.walk(extract_dir):
+                                            for file in files:
+                                                if file.lower().endswith(".dcm"):
+                                                    dcm_path = os.path.join(download_path, root, file)
+                                                    ds = pydicom.dcmread(dcm_path)
+                                                    temperature = ds.get("PatientComments", None)
+                                                    temp_d = re.findall(r'\d+', temperature)  
+                                                    
+                                                    #print("Temperature: ", temp_d)
+                                                    
+                                
+                                else:
+                                    ds = pydicom.dcmread(os.path.join(download_path, fisp_f.name))
+                                    temperature = ds.get("PatientComments", None)
+                                    temp_d = re.findall(r'\d+', temperature)                 
+                                    #print("Temperature: ", temp_d)
             
             except Exception as e:
-                            print("Exception caught ", e)
-                            continue                        
+                print("Exception caught ", e)
+                continue                        
 
 
                 
